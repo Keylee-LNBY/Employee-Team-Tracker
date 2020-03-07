6e6050545f47b0fd8f26e69ab1b46b493531c764
Keylee <lineberry.kelly@gmail.com>
2020-03-07 11:13:31 -0700

    created connection

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+// Global Variables
+let roles;
+let managers;
+
+//Create Connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    //a console.log(of the EMPLOYEE WELCOME MESSAEGE)
+});
