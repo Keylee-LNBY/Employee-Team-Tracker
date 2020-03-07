5c3c1248a4a360f97ff9a13c83a59fc01ecf0f55
Keylee <lineberry.kelly@gmail.com>
2020-03-07 12:50:06 -0700

    added prompt functionality and code to communicate from the app.js to the sql db

--- app.js
+++ app.js
@@ -68,7 +68,65 @@ const addEmployee = () => {
     let query = "SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS managers FROM employee";
     connection.query(query, function (err, res) {
         managers = res;
-        employeeGenerator(roles, managers);
+        addNewEmployee(roles, managers);
+    });
+};
+//The prompts for the information needed to add a new employee to the SQL db
+const addNewEmployee = (roles, managers) => {
+    //Arrays for the roles listed in the array - used in prompts
+    let roleOptions = [];
+    let managerOptions = [];
+    //for loops to push information to arrays
+    for (i = 0; i < roles.length; i++) {
+        roleOptions.push(Object.values(roles[i].title).join(""));
+    };
+    for (i = 0; i < managers.length; i++) {
+        managerOptions.push(Object.values(managers[i].managers).join(""));
+    };
+    //What is prompted to the User
+    inquirer.prompt([
+        {
+        message: "Enter first name of employee:",
+        name: "first_name",
+        type: "input"
+        },
+        {
+        message: "Enter last name of employee:",
+        name: "last_name",
+        type: "input"
+        },
+        {
+        message: "Enter employee's role:",
+        name: "role_id",
+        choices: roleOptions,
+        type: "list"
+        },
+        {
+        message: "Who is the employee's manager?",
+        name: "manager_id",
+        choices: managerOptions,
+        type: "list"
+        }
+    ]).then((res) => {
+        let role_id;
+        let manager_id;
+        //Grabs role_id from assigned role
+        for (i = 0; i < roles.length; i++) {
+            if (roles[i].title === res.role_id) {
+                role_id = roles[i].id;
+            };
+        };
+        for (i = 0; i < managers.length; i++) {
+            if (managers[i].managers === res.manager_id) {
+                manager_id = managers[i].id;
+            };
+        };
+
+        let query = "INSERT INTO employee SET ?, ?, ?, ?";
+        connection.query(query, [{ first_name: res.first_name }, { last_name: res.last_name }, { role_id: role_id }, { manager_id: manager_id }], function (err, res) {
+        if (err) throw err;
+        initApplication();
+        });
     });
 };
 //Update an Employee's Role
